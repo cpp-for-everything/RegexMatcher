28413a95828ecc429956e3e20912b4d46b186211
Alex Tsvetanov <alex.tsvetanov0+github@gmail.com>
2023-10-23 01:27:35 +0300

    captures

--- regex_trie.cpp
+++ regex_trie.cpp
@@ -86,12 +86,14 @@ private:
         char_t(char symbol) { this->symbol = symbol; this->wildcard = false; }
         char_t() : symbol('\0'), wildcard(true) { }
 
-        bool is_any() const { return wildcard; }
+        bool is_any() const { return symbol == '\0' && wildcard; }
         char get() const { return symbol; }
 
-        bool operator<(const char_t& x) const { return (symbol == x.symbol) ? (wildcard < x.wildcard) : (symbol < x.symbol); }       
+        bool operator<(const char_t& x) const { return (symbol == x.symbol) ? (wildcard < x.wildcard) : (symbol < x.symbol); }  
+        bool operator==(const char_t& x) const { return (symbol == x.symbol) && (wildcard == x.wildcard); }       
     };
     static const char_t any;
+    static const char_t placeholder;
 public:
     using node_ptr = Node*;
     using ref_ptr = const Regex*;
@@ -115,20 +117,19 @@ public:
     }
 
     void init(const ref_ptr regex_obj) {
-        std::vector<node_ptr> leafs;
-        this->init(regex_obj->regex, regex_obj, 0, regex_obj->regex.size(), nullptr, &leafs);
-        std::cout << "Generated leafs: " << leafs.size() << std::endl;
+        node_ptr leaf = this->init(regex_obj->regex, regex_obj, 0, regex_obj->regex.size(), nullptr, nullptr);
     }
 
-    void init(const std::string& regex, const ref_ptr ref, size_t start, size_t end, node_ptr parent, std::vector<node_ptr>* leafs) {
+    node_ptr init(const std::string& regex, const ref_ptr ref, size_t start, size_t end, node_ptr parent, node_ptr leaf) {
         this->parent = parent;
         node_ptr current = this;
-        // global leaf
-            node_ptr leaf = new Node();
+        if (leaf == nullptr) {
+            leaf = new Node();
             leaf->endpoint = ref;
+        }
         // temporary elements needed for the capture
-            node_ptr last_tmp = nullptr;
-            std::vector<node_ptr> last_tmp_leafs;
+            node_ptr last_tmp = nullptr; // root of tmp Node
+            node_ptr last_tmp_leaf = nullptr; // leaf of tmp Node
         // temporary elements needed for the repeat
             node_ptr start_of_the_last_repeat = nullptr;
         std::stack<char> brakets;
@@ -152,10 +153,11 @@ public:
                 // parse the capturing group content
                 if (local_expression_end != end - 1) { // there's something after the capturing group so don't include it just yet
                     last_tmp = new Node();
-                    last_tmp->init(regex, nullptr, i + 1, local_expression_end, current, &last_tmp_leafs); // skip opening and closing capturing group brakets
+                    last_tmp_leaf = new Node();
+                    last_tmp->init(regex, nullptr, i + 1, local_expression_end, current, last_tmp_leaf); // skip opening and closing capturing group brakets
                 }
                 else { // there's nothing after the capturing group so just attach it to the current node
-                    current->init(regex, ref, i + 1, local_expression_end, current, leafs); // skip opening and closing capturing group brakets
+                    return current->init(regex, ref, i + 1, local_expression_end, current, leaf); // skip opening and closing capturing group brakets
                 }
                 
                 // move past the capturing group
@@ -164,7 +166,10 @@ public:
             else if (regex[i] == '[') { // char set begins
                 // add the last_tmp to the current node and move the current node
                 if (last_tmp != nullptr) {
-                    // TODO: ...
+                    for(auto x : last_tmp->children) {
+                        current->add_child(x.first, x.second);
+                    }
+                    current = last_tmp_leaf;
                     last_tmp = nullptr;
                 }
                 // create the node after the end of the char set
@@ -191,17 +196,33 @@ public:
                 current = after_the_set;
             }
             else if (regex[i] == '+') {
-                if (last_tmp != nullptr) { // the repeat operator is for a capture
-                    // TODO: ...
+                if (last_tmp != nullptr) {
+                    for(auto x : last_tmp->children) {
+                        current->add_child(x.first, x.second);
+                    }
+                    current = last_tmp_leaf;
+                    current->create_repeater(last_tmp, new int(1), nullptr);
                     last_tmp = nullptr;
                 }
                 else { // the repeat operator is for a char set or a single char
                     current->create_repeater(current->parent, new int(1), nullptr);
                 }
+                if (i == end - 1) {
+                    std::cout << "Is leaf nullptr " << (leaf == nullptr) << std::endl;
+                    current->add_child(placeholder, leaf);
+                    std::cout << leaf->endpoint->uid << std::endl;
+                }
             }
             else if (regex[i] == '*') { }
             else if (regex[i] == '?') { }
             else if (regex[i] == '.') { // wildcard
+                if (last_tmp != nullptr) {
+                    for(auto x : last_tmp->children) {
+                        current->add_child(x.first, x.second);
+                    }
+                    current = last_tmp_leaf;
+                    last_tmp = nullptr;
+                }
                 node_ptr new_node;
                 if (i != end - 1) {
                     new_node = new Node();
@@ -224,7 +245,9 @@ public:
                 current = new_node;
             }
         }
+        return leaf;
     }
+    
     void add_child(char_t key, node_ptr next) {
         if (this->children.find(key) != this->children.end()) {
             std::cout << "The key is not new" << std::endl;
@@ -232,25 +255,35 @@ public:
         }
         this->children.emplace(key, next);
     }
+    
     void create_repeater(node_ptr return_to, int* a, int* b) {
         int _a = a!=nullptr ? *a : -1;
         int _b = b!=nullptr ? *b : -1;
         std::pair<node_ptr, RepeatLimits>* new_element = new std::pair<node_ptr, RepeatLimits>(return_to, RepeatLimits(a, b));
         this->repeaters.push_back(new_element);
     }
+    
     void printDFS(size_t i = 1) {
         for (auto x : children) {
-            if (x.second == nullptr) continue;
-            if (!x.first.is_any())
+            //if (x.second == nullptr) continue;
+            if (!x.first.wildcard && x.first.get() != '\0')
                 std::cout << std::string(i, '\t') << x.first.get() << std::endl;
+            else if (x.first.get() == '\0') {
+                std::cout << std::string(i + 1, '\t') << "(placeholder) " << x.second->endpoint->uid << std::endl;
+                x.second->printDFS(i + 2);
+            }
             else
                 std::cout << std::string(i,'\t') << "<- wildcard" << std::endl;
             for(auto y : x.second->repeaters) {
                 for(auto z : y->first->children) {
                     if (!z.first.is_any())
-                        std::cout << std::string(i + 1,'\t') << "<-" << z.first.get() << std::endl;
+                        std::cout << std::string(i + 1, '\t') << "<-" << z.first.get() << std::endl;
+                    else if (z.first == placeholder) {
+                        std::cout << std::string(i + 1, '\t') << "(placeholder)" << std::endl;
+                        z.second->printDFS(i + 2);
+                    }
                     else
-                        std::cout << std::string(i + 1,'\t') << "<- wildcard" << std::endl;
+                        std::cout << std::string(i + 1, '\t') << "<- wildcard" << std::endl;
                 }
             }
 
@@ -264,8 +297,12 @@ public:
         //std::map<node_ptr, std::map<node_ptr, std::map<RepeatLimits
         if (auto it = children.find(char_t(text[0])); it != children.end())
             q.push(std::make_pair(it->second, 1));
+
         if (auto it = children.find(Node::any); it != children.end())
             q.push(std::make_pair(it->second, 1));
+
+        if (auto it = children.find(Node::placeholder); it != children.end())
+            q.push(std::make_pair(it->second, 0));
         
         while(!q.empty()) {
             const auto front = q.front(); q.pop();
@@ -273,7 +310,12 @@ public:
             const size_t index_to_find = front.second;
 
             if (index_to_find == text.size()) { // everything got matched
-                return front_node->endpoint;
+                if (front_node->endpoint != nullptr)
+                    return front_node->endpoint;
+                else if (auto it = front_node->children.find(Node::placeholder); it->second->endpoint != nullptr)
+                    return it->second->endpoint;
+                else 
+                    return nullptr;
             }
             
             // TODO: check for minimum repeat count if any
@@ -282,6 +324,9 @@ public:
             
             if (auto it = front_node->children.find(Node::any); it != front_node->children.end())
                 q.push(std::make_pair(it->second, index_to_find + 1));
+
+            if (auto it = front_node->children.find(Node::placeholder); it != front_node->children.end())
+                q.push(std::make_pair(it->second, index_to_find));
 
             // TODO: check for maximum repeat count if any
             for (size_t i = 0 ; i < front_node->repeaters.size() ; i ++) {
@@ -326,14 +371,15 @@ private:
 };
 
 const Node::char_t Node::any = char_t();
+const Node::char_t Node::placeholder = char_t('\0');
 
 int main() {
     const Regex* patterns = new Regex[5] {
-        Regex("(a.)", 0),
+        Regex("(a)+", 0),
         Regex("a*c", 1),
         Regex("a*b", 2),
         Regex("[a-c]{1,3}", 3),
-        Regex("([a-z]+.)", 4)
+        Regex("([a-c]+.)", 4)
     };
 
     Node root0, root4;
@@ -361,6 +407,12 @@ int main() {
     else
         std::cout << patterns[0].regex << " ^ " << text << " = no match" << std::endl; 
 
+    text = "aaaa";
+    if (auto it = root0.find(text); it != nullptr)
+        std::cout << patterns[0].regex << " ^ " << text << " = match" << std::endl; 
+    else
+        std::cout << patterns[0].regex << " ^ " << text << " = no match" << std::endl; 
+
     text = "ac";
     if (auto it = root4.find(text); it != nullptr)
         std::cout << patterns[4].regex << " ^ " << text << " = match" << std::endl; 
@@ -383,11 +435,12 @@ int main() {
 /*
 Single regex operations:
     wildcard - done
-    captures - 
+    captures - done
     sets     - done
     +        - done
     *
     ?
+    |
 Multiple regex operations:
     merge 2 nodes
 */
