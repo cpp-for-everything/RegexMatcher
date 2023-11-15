40b39b18553bd32f067ef185a71ba132fc22577f
Alex Tsvetanov <alex.tsvetanov0+github@gmail.com>
2023-11-15 02:00:59 +0200

    benchmarks added

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,6 +5,12 @@
 #define enable(x,y) x.push_back(y);
 #define disable(x,y)
 
+#include <chrono>
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
+
 int main(int argc, char** argv) {
     {
         RegexMatcher<int, char> root;
@@ -90,16 +96,37 @@ int main(int argc, char** argv) {
 
         for (std::string text : texts){
             std::cout << "Matching '" << text << "' resulted in:\n";
-            for (auto x : root.match(text)) {
-                std::cout << "\t" << x << ") " << regexes[x - 1];
-                if (std::regex_search(text, std::regex("^" + regexes[x - 1] + "$"))) {
-                    std::cout << " - Correct\n";
-                }
-                else  {
-                    std::cout << " - Incorrect\n";
+            auto t1 = high_resolution_clock::now();
+            const auto answer = root.match(text);
+            auto t2 = high_resolution_clock::now();
+            std::vector<int> test_result;
+            size_t ind = 0;
+
+            for (size_t i = 0 ; i < regexes.size() ; i ++) {
+                if (std::regex_search(text, std::regex("^(" + regexes[i] + ")$"))) {
+                    test_result.push_back(i + 1);
                 }
             }
-            std::cout << std::endl;
+            auto t3 = high_resolution_clock::now();
+            if (answer.size() != test_result.size()) {
+                std::cout << "\tFailed:\n";
+                for (auto x : answer) {
+                    std::cout << "\t\t     My: " << x << ") " << regexes[x - 1] << std::endl;
+                }
+                std::cout << "\t\t-----------------------\n";
+                for (auto x : test_result) {
+                    std::cout << "\t\t Actual: " << x << ") " << regexes[x - 1] << std::endl;
+                }   
+            }
+            else {
+                bool failed = false;
+                for (size_t i = 0 ; i < answer.size() ; i ++) {
+                    if (failed = (failed || answer[i] != test_result[i]))
+                        std::cout << "\t\t Failed: " << answer[i] << ") " << regexes[answer[i] - 1] << " | " << test_result[i] << ") " << regexes[test_result[i] - 1] << std::endl;
+                }
+                if (!failed)
+                    std::cout << "\t\t Success " << duration<double, std::milli>(t2 - t1).count() << "ms (my) vs " << duration<double, std::milli>(t3 - t2).count() << "ms (O(n^2))" << std::endl;
+            }
             //getchar();
         }
     }
